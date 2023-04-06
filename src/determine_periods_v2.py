1d8a7352a79a9fc01f9730234b463db9ff456769
daniloceano <danilo.oceano@gmail.com>
2023-04-05 21:34:53 -0300

    Feat: extended periods duration to6h, causing overlaps on them

--- src/determine_periods_v2.py
+++ src/determine_periods_v2.py
@@ -122,6 +122,17 @@ def get_peaks_valleys(function):
     df_valleys = pd.Series(valleys,index=times_valleys)
     return df_peaks, df_valleys
 
+def filter_peaks_valleys(series):
+    # Remove peaks and valleys that occured very close to each other
+    min_time_diff = pd.Timedelta(days=1)
+    
+    valid_indices = [series.index[0]]
+    for i in range(1, len(series)):
+        if series.index[i] - series.index[i-1] > min_time_diff:
+            valid_indices.append(series.index[i])
+    return series.loc[valid_indices]
+    
+
 def plot_didatic(da, fname):
 
     colors = ['k', '#134074', '#d62828', '#f7b538', '#5b8e7d',]
@@ -133,11 +144,13 @@ def plot_didatic(da, fname):
     
     plt.close('all')
     fig = plt.figure(figsize=(15, 15))
+    gs = gridspec.GridSpec(3, 3)
     
 # =============================================================================
 #   First figure: all series
 # =============================================================================
-    ax = fig.add_subplot(331,frameon=True)
+    ax = fig.add_subplot(gs[0, 0],frameon=True)
+    
     ax.plot(da.time,dz3,c=colors[3], linewidth=0.75,
               label=r'$\frac{∂^{3}ζ}{∂t^{3}}$')
     ax.plot(da.time,dz2,c=colors[2], linewidth=0.75,
@@ -145,97 +158,79 @@ def plot_didatic(da, fname):
     ax.plot(da.time,dz,c=colors[1], linewidth=0.75,
              label=r'$\frac{∂ζ}{∂t}$')
     ax.plot(da.time,z,c=colors[0], linewidth=2, label='ζ')
-    ax.legend(loc='upper center', bbox_to_anchor=(1.67, 1.3),ncol=4,
+    
+    ax.set_title('Plot ζ and its derivatives ')
+    ax.legend(loc='upper center', bbox_to_anchor=(1.67, 1.45),ncol=4,
               fontsize=16)
     
 # =============================================================================
 #   Get peaks and vallys of the derivatives
 # =============================================================================
-    ax2 = fig.add_subplot(332,frameon=True)
+    ax2 = fig.add_subplot(gs[0, 1],frameon=True)
 
     c = 1
     for function in [dz, dz2, dz3]: 
         peaks, valleys = get_peaks_valleys(function)
             
         for i in range(len(peaks)):
-            ax2.scatter(peaks.index[i],peaks[i],facecolor='#ab791b',
-                        edgecolor='k', linewidth=2)
+            ax2.scatter(peaks.index[i],peaks[i],facecolor=colors[c],
+                        edgecolor='k', linewidth=2, s=150)
         for i in range(len(valleys)):
-            ax2.scatter(valleys.index[i],valleys[i],facecolor='#ab791b',
-                        linewidth=2)
-    
-        negative_dz = function.where(function < 0)
-        positive_dz = function.where(function > 0)
-        ax2.plot(negative_dz.time,negative_dz,c=colors[c], linewidth=1,
-                  linestyle='dashed')
-        ax2.plot(positive_dz.time,positive_dz,c=colors[c], linewidth=1,
+            ax2.scatter(valleys.index[i],valleys[i],facecolor=colors[c],
+                        linewidth=2, s=150)
+        ax2.plot(function.time,function,c=colors[c], linewidth=1,
                   linestyle='-')
         c+=1
     
     ax2.plot(da.time,z,c=colors[0], linewidth=2)
+    ax2.set_title('         Get peaks and valleys in derivatives')
     
 # =============================================================================
 #   Separete data between peaks and valleys of the third derivative
 # =============================================================================
-    ax3 = fig.add_subplot(333,frameon=True)
+    ax3 = fig.add_subplot(gs[0, 2],frameon=True)
     
-    peaks, valleys = get_peaks_valleys(dz3)
-    
-    # Remove peaks and valleys that occured very close to each other
-    min_time_diff = pd.Timedelta(days=1)
-    
-    valid_indices = [peaks.index[0]]
-    for i in range(1, len(peaks)):
-        if peaks.index[i] - peaks.index[i-1] > min_time_diff:
-            valid_indices.append(peaks.index[i])
-    peaks_filtered = peaks.loc[valid_indices]
-    
-    valid_indices = [valleys.index[0]]
-    for i in range(1, len(valleys)):
-        if valleys.index[i] - valleys.index[i-1] > min_time_diff:
-            valid_indices.append(valleys.index[i])
-    valleys_filtered = valleys.loc[valid_indices]
-    
-    for tpeak in peaks_filtered.index:
-        ax3.scatter(tpeak, z.where(z.time==tpeak).dropna(dim='time'),
-                    c=colors[3], edgecolor='k')
-    for tvalley in valleys_filtered.index:
-        ax3.scatter(tvalley, z.where(z.time==tvalley).dropna(dim='time'),
-                    c=colors[3])
-    
-    ax3.plot(da.time,z,c=colors[0], linewidth=2)
-    
-    c = 1
-    for derivative in [dz, dz2, dz3]:
-        negative_dz = derivative.where(derivative < 0)
-        positive_dz = derivative.where(derivative > 0)
+    c=1
+    for function in [dz, dz2, dz3]:
+        peaks, valleys = get_peaks_valleys(function)
         
-        negative_z = z.where(z.time == negative_dz.dropna(dim='time').time)
-        positive_z = z.where(z.time == positive_dz.dropna(dim='time').time)
+        peaks_filtered = filter_peaks_valleys(peaks)
+        valleys_filtered = filter_peaks_valleys(valleys)
         
-        ax3.scatter(negative_z[::4].time,negative_z[::4] + (np.abs(z.min()/8)*c),
-                    c='None', edgecolor=colors[c])
-        ax3.scatter(positive_z[::4].time,positive_z[::4] + (np.abs(z.min()/8)*c),
-                    c=colors[c])
+        for tpeak in peaks_filtered.index:
+            ax3.scatter(tpeak, z.where(z.time==tpeak).dropna(dim='time'),
+                        c=colors[c], edgecolor='k', s=150)
+        for tvalley in valleys_filtered.index:
+            ax3.scatter(tvalley, z.where(z.time==tvalley).dropna(dim='time'),
+                        c=colors[c], s=150)
         c+=1
+        
+    ax3.plot(da.time,z,c=colors[0], linewidth=2)
+    ax3.set_title('Filter peaks closer than 1 day\n and transpose to ζ series')
         
 # =============================================================================
 #   Identify mature stages
 # =============================================================================
-    ax4 = fig.add_subplot(334,frameon=True)
+    ax4 = fig.add_subplot(gs[1, 0],frameon=True)
     
     # Mature stage will be defined as all points between a consecutive
     # maximum and minimum of the third vorticity!
-    peaks = peaks_filtered.replace(peaks.values, 'peak')
-    valleys = valleys_filtered.replace(valleys.values, 'valley')
-    dz3_peaks_valleys = pd.concat([peaks,valleys]).sort_index()
+    dz3_peaks, dz3_valleys = get_peaks_valleys(dz3)
+    dz3_peaks_filtered = filter_peaks_valleys(dz3_peaks)
+    dz3_valleys_filtered = filter_peaks_valleys(dz3_valleys)
+    z_dz3_peaks = z.sel(time=dz3_peaks_filtered.index)
+    z_dz3_valleys = z.sel(time=dz3_valleys_filtered.index)
     
-    dz2_peaks, dz2_valleys = get_peaks_valleys(dz2)
-    z_dz2_peaks = z.sel(time=dz2_peaks.index)
-    z_dz2_valleys = z.sel(time=dz2_valleys.index)
+    peaks = dz3_peaks_filtered.replace(dz3_peaks_filtered.values,
+                                       'peak')
+    valleys = dz3_valleys_filtered.replace(dz3_valleys_filtered.values,
+                                           'valley')
+    dz3_peaks_valleys = pd.concat([peaks,valleys]).sort_index()
     
     dt = z.time[1] - z.time[0]
     dt = pd.Timedelta(dt.values)
+    
+    six_hours = pd.Timedelta('6H')
     
     mature = []
     for i in range(len(dz3_peaks_valleys[:-1])):
@@ -250,28 +245,35 @@ def plot_didatic(da, fname):
     for series in mature:
         ax4.fill_betweenx((z.min(),z.max()), series[0], series[-1],  
                           facecolor=colors[3], alpha=0.6)
-    ax4.scatter(z_dz2_peaks.time,z_dz2_peaks,facecolor=colors[2], edgecolor='k')
-    ax4.scatter(z_dz2_valleys.time,z_dz2_valleys,facecolor=colors[2])
+    ax4.scatter(z_dz3_peaks.time,z_dz3_peaks,facecolor=colors[3],
+                edgecolor='k', s=150)
+    ax4.scatter(z_dz3_valleys.time,z_dz3_valleys,facecolor=colors[3], s=150)
     
     ax4.set_title('Mature stage')
     
 # =============================================================================
 #   Identifying intensification and incipient stages stages
 # =============================================================================
-    ax5 = fig.add_subplot(335,frameon=True)
+    ax5 = fig.add_subplot(gs[1, 1],frameon=True)
     
+    dz2_peaks, dz2_valleys = get_peaks_valleys(dz2)
+    z_dz2_peaks = z.sel(time=dz2_peaks.index)
+    z_dz2_valleys = z.sel(time=dz2_valleys.index)
+        
     # If thre is a minimum of the second derivative before the start of 
     # mature phase, the intensification starts there. Else, there is an
     # incipient stage
     intensification = []
     for series, i in zip(mature, range(len(mature))):
-        mature_start = series[0]
-        mature_end = series[-1]
+        mature_start = series[0] 
+        mature_end = series[-1] 
         if i == 0:
             if dz2_valleys.index[i] < mature_start:
                 intensification_start = dz2_valleys.index[i]
-                incipient = z.time.sel(time=slice(z.time[0],
-                                                  intensification_start-dt))
+                incipient = pd.date_range(z.time[0],intensification_start-dt,
+                                freq=f'{int(dt.total_seconds() / 3600)} H')
+                # incipient = z.time.sel(time=slice(z.time[0],
+                #                                   intensification_start-dt))
             else:
                 intensification_start = (z.time[0]).values
                 incipient = []
@@ -289,16 +291,22 @@ def plot_didatic(da, fname):
         ax5.fill_betweenx((z.min(),z.max()), series[0],
                       series[-1], facecolor=colors[2], alpha=0.6)
         
-    ax5.scatter(z_dz2_peaks.time,z_dz2_peaks,facecolor=colors[2], edgecolor='k')
-    ax5.scatter(z_dz2_valleys.time,z_dz2_valleys,facecolor=colors[2])
+    ax5.scatter(z_dz3_peaks.time,z_dz3_peaks,facecolor=colors[3],
+                edgecolor='k', s=150)
+    ax5.scatter(z_dz2_peaks.time,z_dz2_peaks,facecolor=colors[2],
+                edgecolor='k', s=150)
+    ax5.scatter(z_dz2_valleys.time,z_dz2_valleys,facecolor=colors[2], s=150)
     
     ax5.plot(da.time,z,c=colors[0], linewidth=2)
     ax5.set_title('Intensification stage')
     
-    # Plot incipient stage, if there's any
-    ax6 = fig.add_subplot(336,frameon=True)
-    ax6.scatter(z_dz2_peaks.time,z_dz2_peaks,facecolor=colors[2], edgecolor='k')
-    ax6.scatter(z_dz2_valleys.time,z_dz2_valleys,facecolor=colors[2])
+# =============================================================================
+#   Plot incipient stage, if there's any
+# =============================================================================
+    ax6 = fig.add_subplot(gs[1, 2],frameon=True)
+    ax6.scatter(z_dz2_peaks.time,z_dz2_peaks,facecolor=colors[2],
+                edgecolor='k', s=150)
+    ax6.scatter(z_dz2_valleys.time,z_dz2_valleys,facecolor=colors[2], s=150)
     if len(incipient) > 0:
         ax6.fill_betweenx((z.min(),z.max()), incipient[0],
                   incipient[-1], facecolor=colors[1], alpha=0.6)
@@ -308,8 +316,7 @@ def plot_didatic(da, fname):
 # =============================================================================
 #   Get decaying stages
 # =============================================================================
-
-    ax7 = fig.add_subplot(337,frameon=True)
+    ax7 = fig.add_subplot(gs[2, 0],frameon=True)
     decaying = []
     
     if len(mature) == 1:
@@ -335,30 +342,44 @@ def plot_didatic(da, fname):
         if len(series) > 0:
             ax7.fill_betweenx((z.min(),z.max()), series[0], series[-1],  
                               facecolor=colors[4], alpha=0.6)
-    ax7.scatter(z_dz2_peaks.time,z_dz2_peaks,facecolor=colors[2], edgecolor='k')
-    ax7.scatter(z_dz2_valleys.time,z_dz2_valleys,facecolor=colors[2])    
+    ax7.scatter(z_dz2_peaks.time,z_dz2_peaks,facecolor=colors[2],
+                edgecolor='k', s=150)
+    ax7.scatter(z_dz2_valleys.time,z_dz2_valleys,facecolor=colors[2], s=150) 
+    ax7.scatter(z_dz3_valleys.time,z_dz3_valleys,facecolor=colors[3], s=150)
         
     ax7.plot(da.time,z,c=colors[0], linewidth=2)
-    ax7.set_title('Decaying stage stage')
-
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-%d'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    plt.gcf().autofmt_xdate()
-    # plt.subplots_adjust(right=0.85)
+    ax7.set_title('Decaying stage')
     
 # =============================================================================
 #   Plot everything together
 # =============================================================================
-    ax8 = fig.add_subplot(3,2,6,frameon=True)  
+    ax8 = fig.add_subplot(gs[2, 1:3],frameon=True)
     
-    for phase, c in zip([[incipient], intensification, mature, decaying],
+    phases = {}
+    for phase, key in zip([[incipient], intensification, mature, decaying],
+                    ['incipient', 'intensification', 'mature', 'decaying']):
+        tmp = []
+        print(key)
+        for period in phase:
+            if len(period) > 0:
+                tmp.append(pd.date_range(
+                    period[0]-six_hours, period[-1]+six_hours,
+                              freq=f'{int(dt.total_seconds() / 3600)} H'))
+        phases[key] = tmp
+        
+    for phase, c in zip(phases.keys(),
                           ['#134074','#d62828','#f7b538','#5b8e7d']):
-        for series in phase:
+        for series in phases[phase]:
             if len(series) > 0:
-                ax8.fill_betweenx((z.min(),z.max()), series[0], series[-1],  
+                ax8.fill_betweenx((z.min(),z.max()), series[0],series[-1],  
                                   facecolor=c, alpha=0.6)
     
     ax8.plot(da.time,z,c=colors[0], linewidth=2)
+    ax8.set_title('Add overlaps for six hours between periods as confidence interval')
+    
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    plt.gcf().autofmt_xdate()
 
     outname = '../vorticity_analysis/analysis/'+fname
     outname+='.png'
@@ -375,7 +396,7 @@ LatIndexer = dfVars.loc['Latitude']['Variable']
 TimeIndexer = dfVars.loc['Time']['Variable']
 LevelIndexer = dfVars.loc['Vertical Level']['Variable']    
 
-for testfile in glob.glob(data_dir+'/*'):   
+for testfile in glob.glob(data_dir+'/*')[:1]:   
         
     fname = testfile.split('/')[-1].split('.nc')[0] 
     id_cyclone = fname.split('_')[0]
