5b83fd53afb55c88bd00759335b4315818ce67b2
daniloceano <danilo.oceano@gmail.com>
2023-06-21 17:30:14 -0300

    Update: download ERA5 data

---
+++ src/GetERA5-date_limits.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 19:40:27 2023
+
+@author: danilocoutodsouza
+"""
+
+import subprocess
+import fileinput
+import shutil
+import pandas as pd
+import os 
+
+def download_ERA5(infile, prefix, scripts_dir, data_dir):
+    """
+    Downloads ERA5 data files for a given list of files and date ranges.
+    
+    infile (srt):
+      Path to the input file containing a list of files and date ranges.
+    prefix (str):
+      Prefix for the output file names.
+    scripts_dir (str):
+      Path to the directory where the scripts should be saved.
+    data_dir (str):
+      Path to the directory where the data should be saved.
+    """
+
+    with open(infile, 'r') as f:
+        next(f)  # Skip the first line
+        for line in f:
+            
+            file_id, date_start, date_end, south, north, west, east = line.strip().split(',')
+            
+            shutil.copy('GetERA5-pl.py', f'GetERA5-pl_{file_id}.py')
+            
+            # Increase area size a little bit for avoinding data too close to boundaries
+            north = round(float(north)+8)
+            south = round(float(south)-8)
+            east = round(float(east)+8)
+            west = round(float(west)-8)
+            
+            # Get dates and hours as distinct variables
+            day_start = date_start.split()[0]
+            day_start_fmt = pd.to_datetime(day_start).strftime('%Y-%m-%d')
+            day_end = date_end.split()[0]
+            day_end_fmt = pd.to_datetime(day_end).strftime('%Y-%m-%d')
+            
+            # Perform replacements in the script file
+            script_file = f'GetERA5-pl_{file_id}.py'
+            outfile = f'{prefix}-{file_id}_ERA5.nc'
+            for line in fileinput.input(script_file, inplace=True):
+                if "'date':" in line:
+                    print(f"        'date': '{day_start_fmt}/{day_end_fmt}',")
+                elif "'area':" in line:
+                    print(f"        'area': '{north}/{west}/{south}/{east}',")
+                elif "'ID_ERA5.nc'" in line:
+                    print(f"    '{outfile}')")
+                else:
+                    print(line, end='')
+
+            # Download file
+            cmd = ['python', script_file, file_id]
+            subprocess.call(cmd)
+
+            # Remove existing script file if it exists in the destination directory
+            script_dest = os.path.join(scripts_dir, script_file)
+            if os.path.exists(script_dest):
+                print(f"{script_file} already exists in {script_dest}. Removing it.")
+                os.remove(script_dest)
+                
+            # Move the new script file to the destination directory, if it exists
+            if os.path.exists(script_file):
+                shutil.move(script_file, scripts_dir)
+                print(f"moved {script_file} to {scripts_dir}")
+
+            # Check if outfile already exists
+            outfile_path = os.path.join(data_dir, outfile)
+            if os.path.exists(outfile_path):
+                print(f"Destination path '{outfile_path}' already exists. Overwriting the file.")
+                # Overwrite the outfile
+                os.remove(outfile_path)
+                shutil.move(outfile, outfile_path, copy_function=shutil.copy2)
+            else:
+                # Move the outfile to the data directory if it was downloaded
+                if os.path.exists(outfile):
+                    shutil.move(outfile, outfile_path)
+                    print(f"Moved {outfile} to {data_dir}")
+                else:
+                    print(f'{outfile} not downloaded.')
