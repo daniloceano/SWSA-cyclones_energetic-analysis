d29b0d398aed8b06754141c4716d703efa3e27fe
daniloceano <danilo.oceano@gmail.com>
2023-08-01 10:28:45 -0300

    Update: csv with energetics

--- src_determine-patterns/periods_energetics-RG.py
+++ src_determine-patterns/periods_energetics-RG.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    periods_energetics-RG.py                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: Danilo  <danilo.oceano@gmail.com>          +#+  +:+       +#+         #
+#    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/26 15:02:10 by Danilo            #+#    #+#              #
-#    Updated: 2023/07/31 11:03:22 by Danilo           ###   ########.fr        #
+#    Updated: 2023/07/31 17:13:51 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,23 +28,28 @@ for RG in range(1,4):
         id = result.split('/')[-1].split('_')[0]
 
         periods_file = result+'/periods.csv'
-        results_file = glob.glob(result+'/*_ERA5_track-15x15.csv')[0]
+        try:
+            results_file = glob.glob(result+'/*_ERA5_track-15x15.csv')[0]
         
-        df = pd.read_csv(results_file, index_col=[0])
-        df['Datetime'] = pd.to_datetime(df.Date) + pd.to_timedelta(df.Hour, unit='h')
-        
-        periods = pd.read_csv(periods_file, index_col=[0])
-        periods = periods.dropna()
-        for i in range(len(periods)):
-            start,end = periods.iloc[i]['start'],periods.iloc[i]['end']
-            selected_dates = df[(df['Datetime'] >= start) & (df['Datetime'] <= end)]
-            if i == 0:
-                period = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
-                period = period.to_frame(name=periods.iloc[i].name).transpose()
-            else:
-                tmp = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
-                tmp = tmp.to_frame(name=periods.iloc[i].name).transpose()
-                period = pd.concat([period,tmp]) 
+            df = pd.read_csv(results_file, index_col=[0])
+            df['Datetime'] = pd.to_datetime(df.Date) + pd.to_timedelta(df.Hour, unit='h')
+            
+            periods = pd.read_csv(periods_file, index_col=[0])
+            periods = periods.dropna()
+            for i in range(len(periods)):
+                start,end = periods.iloc[i]['start'],periods.iloc[i]['end']
+                selected_dates = df[(df['Datetime'] >= start) & (df['Datetime'] <= end)]
+                if i == 0:
+                    period = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
+                    period = period.to_frame(name=periods.iloc[i].name).transpose()
+                else:
+                    tmp = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
+                    tmp = tmp.to_frame(name=periods.iloc[i].name).transpose()
+                    period = pd.concat([period,tmp]) 
+
+        except:
+            print(result)
+            pass
             
         fname = f'{outdir}/{id}_ERA5.csv'
         period.to_csv(fname)
