f41260851f69ea69a7ab3cd9b35c4ee74fae027e
daniloceano <danilo.oceano@gmail.com>
2023-08-03 17:59:47 -0300

    Feat: export percentages

--- src_life-cycle/export_species.py
+++ src_life-cycle/export_species.py
@@ -6,7 +6,7 @@
 #    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/03 14:07:51 by Danilo            #+#    #+#              #
-#    Updated: 2023/08/03 16:34:39 by Danilo           ###   ########.fr        #
+#    Updated: 2023/08/03 17:53:32 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,16 +20,19 @@ phase_counts = {}
 seasonal_phase_counts = {season: {} for season in ['DJF', 'MAM', 'JJA', 'SON']}
 
 month_season_map = {
-        12: 'DJF', 1: 'DJF', 2: 'DJF',
-        3: 'MAM', 4: 'MAM', 5: 'MAM',
-        6: 'JJA', 7: 'JJA', 8: 'JJA',
-        9: 'SON', 10: 'SON', 11: 'SON'
-    }
+    12: 'DJF', 1: 'DJF', 2: 'DJF',
+    3: 'MAM', 4: 'MAM', 5: 'MAM',
+    6: 'JJA', 7: 'JJA', 8: 'JJA',
+    9: 'SON', 10: 'SON', 11: 'SON'
+}
+
+total_systems = 0
 
 for csv_file in csv_files:
     df = pd.read_csv(csv_file)
-    if 'residual' in df.columns:
-        df = df.drop(['residual'], axis=1)
+    for residual_phase in ['residual', 'residual 2']:
+        if residual_phase in df.columns:
+            df = df.drop([residual_phase], axis=1)
     phases = list(df.columns)
     phase_arrangement = ', '.join(phases)
     phase_counts[phase_arrangement] = phase_counts.get(phase_arrangement, 0) + 1
@@ -43,16 +46,20 @@ for csv_file in csv_files:
 
     # Find the corresponding season in the month_season_map
     corresponding_season = month_season_map[system_month]
-    
+
     # Count the seasonal occurrences of the current type beginning on the first day of the event
     seasonal_phase_counts[corresponding_season].setdefault(phase_arrangement, 0)
     seasonal_phase_counts[corresponding_season][phase_arrangement] += 1
 
-# Export total count to CSV
+    total_systems += 1
+
+# Export total count and relative percentages to CSV
 total_df = pd.DataFrame(list(phase_counts.items()), columns=['Type of System', 'Total Count'])
+total_df['Percentage'] = total_df['Total Count'] / total_systems * 100
 total_df.to_csv('total_count_of_systems.csv', index=False)
 
-# Export seasonal counts to separate CSV files
+# Export seasonal counts and relative percentages to separate CSV files
 for season in seasonal_phase_counts.keys():
     season_df = pd.DataFrame(list(seasonal_phase_counts[season].items()), columns=['Type of System', 'Count'])
+    season_df['Percentage'] = season_df['Count'] / total_systems * 100
     season_df.to_csv(f'{season}_count_of_systems.csv', index=False)
