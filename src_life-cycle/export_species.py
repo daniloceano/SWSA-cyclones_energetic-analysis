2baac8f5592ef214718482d254a92a4f001993c8
daniloceano <danilo.oceano@gmail.com>
2023-08-03 16:36:26 -0300

    Feat: species count

--- src_life-cycle/export_species.py
+++ src_life-cycle/export_species.py
@@ -6,7 +6,7 @@
 #    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/03 14:07:51 by Danilo            #+#    #+#              #
-#    Updated: 2023/08/03 14:44:18 by Danilo           ###   ########.fr        #
+#    Updated: 2023/08/03 16:34:39 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,8 +17,14 @@ csv_directory = '../periods-energetics/BY_RG-all_raw/'
 csv_files = glob.glob(f'{csv_directory}/*')
 
 phase_counts = {}
-seasons_list = ['Summer', 'Fall', 'Winter', 'Spring']
-seasonal_phase_counts = {season: {} for season in seasons_list}
+seasonal_phase_counts = {season: {} for season in ['DJF', 'MAM', 'JJA', 'SON']}
+
+month_season_map = {
+        12: 'DJF', 1: 'DJF', 2: 'DJF',
+        3: 'MAM', 4: 'MAM', 5: 'MAM',
+        6: 'JJA', 7: 'JJA', 8: 'JJA',
+        9: 'SON', 10: 'SON', 11: 'SON'
+    }
 
 for csv_file in csv_files:
     df = pd.read_csv(csv_file)
@@ -28,24 +34,25 @@ for csv_file in csv_files:
     phase_arrangement = ', '.join(phases)
     phase_counts[phase_arrangement] = phase_counts.get(phase_arrangement, 0) + 1
 
-    # Assuming index contains dates in datetime format
-    df.index = pd.to_datetime(df.index)
+    # Get the month of the system_start
+    if len(df.columns) > 0:
+        system_start = pd.to_datetime(df.iloc[0][0])
+        system_month = system_start.month
+    else:
+        continue
+
+    # Find the corresponding season in the month_season_map
+    corresponding_season = month_season_map[system_month]
     
-    # Categorize seasons based on months
-    df['Season'] = pd.cut(df.index.month, [0, 2, 5, 8, 11], labels=seasons_list)
-    
-    # Count the occurrences of each type of system in each season
-    for season in seasons_list:
-        seasonal_df = df[df['Season'] == season]
-        count = len(seasonal_df)
-        seasonal_phase_counts[season].setdefault(phase_arrangement, 0)
-        seasonal_phase_counts[season][phase_arrangement] += count
+    # Count the seasonal occurrences of the current type beginning on the first day of the event
+    seasonal_phase_counts[corresponding_season].setdefault(phase_arrangement, 0)
+    seasonal_phase_counts[corresponding_season][phase_arrangement] += 1
 
 # Export total count to CSV
 total_df = pd.DataFrame(list(phase_counts.items()), columns=['Type of System', 'Total Count'])
 total_df.to_csv('total_count_of_systems.csv', index=False)
 
 # Export seasonal counts to separate CSV files
-for season, type_counts in seasonal_phase_counts.items():
-    season_df = pd.DataFrame(list(type_counts.items()), columns=['Type of System', 'Count'])
-    season_df.to_csv(f'{season.lower()}_count_of_systems.csv', index=False)
+for season in seasonal_phase_counts.keys():
+    season_df = pd.DataFrame(list(seasonal_phase_counts[season].items()), columns=['Type of System', 'Count'])
+    season_df.to_csv(f'{season}_count_of_systems.csv', index=False)
