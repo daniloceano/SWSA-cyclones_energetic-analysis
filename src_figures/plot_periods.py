6455cd13021b98a60f057b444586a849ddbad6bc
daniloceano <danilo.oceano@gmail.com>
2023-08-29 20:19:38 -0300

    Implemented determine_periods package

--- src_figures/plot_periods.py
+++ src_figures/plot_periods.py
@@ -8,8 +8,9 @@ Created on Fri Feb  3 17:17:29 2023
 
 import glob
 import pandas as pd
-import determine_periods as det
+from cyclophaser import determine_periods
 import matplotlib.pyplot as plt
+import os
 
 intensities = ['0.99']
 output_directory = '../figures/'
@@ -36,21 +37,15 @@ for intensity in intensities:
         print('Cyclone ID:',id_cyclone)
         print('Track file:',track_file) 
 
-        det.check_create_folder(periods_outfile_path)
-        det.check_create_folder(periods_didatic_outfile_path)
+        os.makedirs(periods_outfile_path, exist_ok=True)
+        os.makedirs(periods_didatic_outfile_path, exist_ok=True)
 
         # Set the output file names
         periods_outfile = f"{periods_outfile_path}{id_cyclone}"
         periods_didatic_outfile = f"{periods_didatic_outfile_path}{id_cyclone}"
 
-        # Read the track file and extract the vorticity data
-        track = pd.read_csv(track_file, parse_dates=[0], delimiter=';', index_col=[0])
-        zeta_df = pd.DataFrame(track['min_zeta_850'].rename('zeta'))        
-        vorticity = det.array_vorticity(zeta_df)
-
         # Determine the periods
-        periods_dict, df = det.get_periods(vorticity)
-
-        # Create plots
-        det.plot_all_periods(periods_dict, df, ax=None, vorticity=vorticity.zeta, periods_outfile_path=periods_outfile)
-        det.plot_didactic(df, vorticity, periods_didatic_outfile)
+        df = determine_periods(track_file,
+                                plot=periods_outfile,
+                                plot_steps=periods_didatic_outfile,
+                                export_dict=False)
