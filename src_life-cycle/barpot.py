4847b8bb0797dd305478580e6979adef7b9ba535
Danilo Couto de Souza <danilo.oceano@gmail.com>
2023-09-19 09:44:12 -0300

    Barplots for seasonal counts and RG

--- src_life-cycle/barpot.py
+++ src_life-cycle/barpot.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    barpot.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
+#    By: Danilo  <danilo.oceano@gmail.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/03 16:44:54 by Danilo            #+#    #+#              #
-#    Updated: 2023/08/15 16:58:42 by Danilo           ###   ########.fr        #
+#    Updated: 2023/09/19 09:39:33 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,14 +20,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-def get_filtered_df(df, percentage_threshold=1, num_phases_threshold=0):
-    # Filter rows based on percentage and number of phases
-    filtered_df = df[df['Percentage'] > percentage_threshold]
-    filtered_df['Num Phases'] = filtered_df['Type of System'].apply(lambda x: len(x.split(', ')))
-    filtered_df = filtered_df[filtered_df['Num Phases'] > num_phases_threshold]
-    return filtered_df
+def process_df(df, percentage_threshold=1, filter_df=False, exclude_residual=False):
 
-def plot_barplot(df, season, output_directory, filter=False):
+    processed_df = df.copy()
+
+    # Filter rows based on percentage and number of phases
+    if filter_df:
+        processed_df = df[df['Percentage'] > percentage_threshold]
+        processed_df['Num Phases'] = processed_df['Type of System'].apply(lambda x: len(x.split(', ')))
+    
+    if exclude_residual:
+        # Exclude systems with the 'residual' stage
+        processed_df['Type of System'] = processed_df['Type of System'].str.replace(', residual', '')
+        processed_df = processed_df.groupby('Type of System', as_index=False).agg({
+                    'Total Count': 'sum',
+                    'Percentage': 'sum'
+                    }).sort_values(by='Total Count', ascending=False)
+    
+    return processed_df
+
+def plot_barplot(df, title, fname):
     plt.close('all')
 
     # Mapping labels to the desired scheme
@@ -54,7 +66,7 @@ def plot_barplot(df, season, output_directory, filter=False):
     # Create a bar plot using Seaborn
     plt.figure(figsize=(12, 6))
     sns.barplot(x='Total Count', y='Type of System', data=df, orient='h', ci=None, palette='pastel', edgecolor='grey')
-    plt.title(f'{season} ({total_count} - {total_percentage:.1f}%)', fontweight='bold')
+    plt.title(f'{title} ({total_count} - {total_percentage:.1f}%)', fontweight='bold')
     
     # Add text annotations for total count and percentage on the right side of each bar
     for index, value in enumerate(df['Total Count']):
@@ -71,39 +83,36 @@ def plot_barplot(df, season, output_directory, filter=False):
     plt.tight_layout()
 
     # Save the plot as an image file
-    if filter:
-        output_file = os.path.join(output_directory, f'{season}_filtered.png')
-    else:
-        output_file = os.path.join(output_directory, f'{season}.png')
-    plt.savefig(output_file)
-    print(f'{output_file} saved.')
-    
-def plot_barplots(df, filtered_df, season, output_directory):
-    # Plot bar plot for all systems
-    plot_barplot(df, season, output_directory)
+    plt.savefig(fname)
+    print(f'{fname} saved.')
 
-    # Reset the index of filtered DataFrame before plotting
-    filtered_df = filtered_df.reset_index(drop=True)
+fig_output_directory = '../figures/periods_statistics/barplots/'
+csv_directory = '../periods_species_statistics'
+os.makedirs(fig_output_directory, exist_ok=True)
 
-    # Plot bar plot for filtered systems
-    plot_barplot(filtered_df, season, output_directory, filter=True)
-
-if __name__ == "__main__":
-    output_directory = '../figures/periods_statistics/barplots/'
-    os.makedirs(output_directory, exist_ok=True)
+for RG in ['RG1', 'RG2', 'RG3', 'all_RG']:
 
     # List of season names
     seasons = ['JJA', 'MAM', 'SON', 'DJF', 'total']
 
     for season in seasons:
         # Read data from CSV file
-        df = pd.read_csv(f'{season}_count_of_systems.csv')
+        df = pd.read_csv(f'{csv_directory}/{season}_count_of_systems_{RG}.csv')
         df = df.sort_values(by='Total Count', ascending=False)
 
-        # Assuming 'Percentage' and 'Type of System' columns are present in the DataFrame
-        filtered_df = get_filtered_df(df)
+        # df excluding residual phases
+        df_excluded_residual = process_df(df, filter_df=False, exclude_residual=True)
 
-        if season == 'total':
-            filtered_df['Type of System'].to_csv('filtered_periods.csv', index=False)
+        # df for phases with more than 1%
+        filtered_df = process_df(df, filter_df=True, exclude_residual=False)
 
-        plot_barplots(df, filtered_df, season, output_directory)
+        # df for phases with more than 1% and exluding residual phases
+        filtered_df_exclude_residual = process_df(df, filter_df=True, exclude_residual=True)
+
+        # export csv
+        csv_name = f'{csv_directory}/{season}_count_of_systems_processed_{RG}.csv'
+        filtered_df_exclude_residual.to_csv(csv_name)
+        print(f'{csv_name} saved.')
+
+        fname = f'{fig_output_directory}/{season}_processed_{RG}.png'
+        plot_barplot(filtered_df_exclude_residual, f'{RG} - {season}', fname)
