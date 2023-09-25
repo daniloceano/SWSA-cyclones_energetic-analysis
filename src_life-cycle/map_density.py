e49af1119dacf7e377e7eb420a602ed5c021e008
Danilo Couto de Souza <danilo.oceano@gmail.com>
2023-09-25 14:04:06 -0300

    Panels: map density by season and RG

--- src_life-cycle/map_density.py
+++ src_life-cycle/map_density.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    map_density.py                                     :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
+#    By: Danilo  <danilo.oceano@gmail.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/08 20:33:08 by Danilo            #+#    #+#              #
-#    Updated: 2023/09/21 17:24:30 by Danilo           ###   ########.fr        #
+#    Updated: 2023/09/25 10:08:49 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,16 +24,16 @@ import numpy as np
 import os
 
 def gridlines(ax):
-    # ax.add_feature(cartopy.feature.LAND)
-    # ax.add_feature(cartopy.feature.OCEAN,facecolor=("lightblue"))
     gl = ax.gridlines(draw_labels=True,zorder=2,linestyle='dashed',alpha=0.8,
                  color='#383838')
     gl.xlabel_style = {'size': 14, 'color': '#383838'}
     gl.ylabel_style = {'size': 14, 'color': '#383838'}
-    gl.bottom_labels = None
+    gl.top_labels = None
     gl.right_labels = None
 
-def plot_density(density, fname, output_directory_RG):
+def plot_density(ax, density, phase):
+
+    ax.set_extent([-90, 180, 0, -90], crs=datacrs)
 
     lon, lat = density.lon, density.lat
 
@@ -42,51 +42,55 @@ def plot_density(density, fname, output_directory_RG):
     cf = plt.contourf(lon, lat, density, cmap=cmap, levels=levels)
 
     # Create a separate axis for the colorbar
-    cax = fig.add_axes([0.92, 0.078, 0.02, 0.45]) 
-    cbar = plt.colorbar(cf, cax=cax, ticks=levels) 
+    cax = fig.add_axes([ax.get_position().x1 + 0.02,
+                        ax.get_position().y0, 0.02,
+                        ax.get_position().height]) 
+    # Define the ticks for the colorbar with every 2nd value
+    ticks = np.round(levels[::3], decimals=2)
+    plt.colorbar(cf, cax=cax, ticks=ticks)
 
-    ax.text(-90, -91, phase, ha='left', va='top', fontsize=20, fontweight='bold')
+    props = dict(boxstyle='round', facecolor='white')
+    ax.text(160, -18, phase, ha='right', va='bottom', fontsize=12, fontweight='bold', bbox=props)
 
     ax.coastlines(zorder=1)
     gridlines(ax)
-    plt.tight_layout()
 
-    plt.savefig(os.path.join(output_directory_RG, fname), bbox_inches='tight')
-    print(f'Density map saved in {os.path.join(output_directory_RG, fname)}')
+#####################################    
 
 output_directory = '../figures/periods_statistics/density_maps/'
 infile_directory = '../periods_species_statistics/track_density/'
 
-#####################################
+phases = ['incipient', 'intensification', 'mature', 'decay', 'residual',
+                    'intensification 2', 'mature 2', 'decay 2']
 
 colors = ['white', '#F1F5F9', '#AFC4DA', '#4471B2', '#B1DFA3', '#EFF9A6', 
             '#FEEC9F', '#FDB567', '#F06744',  '#C1274A']
 cmap = mcolors.LinearSegmentedColormap.from_list("", colors)
 
+os.makedirs(output_directory, exist_ok=True)
+
 for RG in ['1', '2', '3', 'all']:
+    RG_str = f'RG{RG}' if RG != 'all' else 'all-RG'
+    print(f'RG: {RG}...')
 
     for season in ['DJF', 'MAM', 'JJA', 'SON', False]:
+        season_str = f'_{season}' if season else ''
+        print(f'season: {season}') if season else print('all seasons') 
 
-        for phase in ['incipient', 'intensification', 'mature', 'decay', 'residual',
-                    'intensification 2', 'mature 2', 'decay 2']:
-            
-            RG_str = f'RG{RG}' if RG != 'all' else 'all-RG'
-            season_str = f'_{season}' if season else ''
+        infile = f'{infile_directory}/track_density_{RG_str}{season_str}.nc'
+        ds = xr.open_dataset(infile)
 
-            fig = plt.figure(figsize=(15, 10))
-            datacrs = ccrs.PlateCarree()
-            ax = fig.add_axes([-0.01, -0.05, 0.9, 0.7], projection=datacrs, frameon=True)
-            ax.set_extent([-90, 180, 0, -90], crs=datacrs)
+        fname = os.path.join(output_directory, RG_str+season_str)
 
+        fig = plt.figure(figsize=(15, 10))
+        datacrs = ccrs.PlateCarree()
+        plt.subplots_adjust(wspace=0.35)
 
-            infile = f'{infile_directory}/track_density_{RG_str}{season_str}.nc'
-            ds = xr.open_dataset(infile)
-
-            output_directory_RG = output_directory + RG_str
-            os.makedirs(output_directory_RG, exist_ok=True)
+        for i, phase in enumerate(phases):
+            ax = fig.add_subplot(4, 2, i+1, projection=datacrs, )
 
             density = ds[phase]
-            fname = f'density_map_{RG_str}{season_str}_{phase}.png'
+            plot_density(ax, density, phase)
 
-            plot_density(density, fname, output_directory_RG)
-
+        plt.savefig(fname, bbox_inches='tight')
+        print(f'Density map saved in {fname}')
