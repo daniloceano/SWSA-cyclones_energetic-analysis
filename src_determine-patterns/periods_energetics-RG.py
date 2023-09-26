84503b2d1a4dd12ba338ff0eeaa29a62e73cdec9
daniloceano <danilo.oceano@gmail.com>
2023-09-26 19:41:59 -0300

    Quick fix

--- src_determine-patterns/periods_energetics-RG.py
+++ src_determine-patterns/periods_energetics-RG.py
@@ -6,7 +6,7 @@
 #    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/26 15:02:10 by Danilo            #+#    #+#              #
-#    Updated: 2023/09/26 19:34:55 by Danilo           ###   ########.fr        #
+#    Updated: 2023/09/26 19:41:50 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -52,31 +52,3 @@ for RG in range(1,4):
         }
 
         df = determine_periods(track_file, **options)
-
-        # periods_file = result+'/periods.csv'
-        # try:
-        #     results_file = glob.glob(result+'/*_ERA5_track-15x15.csv')[0]
-        
-        #     df = pd.read_csv(results_file, index_col=[0])
-        #     df['Datetime'] = pd.to_datetime(df.Date) + pd.to_timedelta(df.Hour, unit='h')
-            
-        #     periods = pd.read_csv(periods_file, index_col=[0])
-        #     periods = periods.dropna()
-        #     for i in range(len(periods)):
-        #         start,end = periods.iloc[i]['start'],periods.iloc[i]['end']
-        #         selected_dates = df[(df['Datetime'] >= start) & (df['Datetime'] <= end)]
-        #         if i == 0:
-        #             period = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
-        #             period = period.to_frame(name=periods.iloc[i].name).transpose()
-        #         else:
-        #             tmp = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
-        #             tmp = tmp.to_frame(name=periods.iloc[i].name).transpose()
-        #             period = pd.concat([period,tmp]) 
-
-        # except:
-        #     print(result)
-        #     pass
-            
-        # fname = f'{outdir}/{id}_ERA5.csv'
-        # period.to_csv(fname)
-        # print(f'{fname} created')
