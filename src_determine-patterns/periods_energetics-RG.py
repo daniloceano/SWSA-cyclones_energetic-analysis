d3e40c5ca019d916837a93f5fc1beaa010c98747
daniloceano <danilo.oceano@gmail.com>
2023-09-26 19:32:46 -0300

    Rmeove old results

--- src_determine-patterns/periods_energetics-RG.py
+++ src_determine-patterns/periods_energetics-RG.py
@@ -6,7 +6,7 @@
 #    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/26 15:02:10 by Danilo            #+#    #+#              #
-#    Updated: 2023/09/26 16:22:40 by Danilo           ###   ########.fr        #
+#    Updated: 2023/09/26 19:30:41 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,40 +16,62 @@ import os
 
 qauntile = 0.99
 
+figures_outdir = f'../figures/periods/quantile/{qauntile}'
+didatic_outdir = f'../figures/periods_didactic/quantile/{qauntile}'
+csv_outdir = f'../periods-energetics/{qauntile}/'
+os.makedirs(figures_outdir, exist_ok=True)
+os.makedirs(csv_outdir, exist_ok=True)
+
 for RG in range(1,4):
 
     results = glob.glob(f'../LEC_results-{qauntile}/RG{RG}-{qauntile}*ERA5*')
-    outdir = f'../periods-energetics/{qauntile}/RG{RG}'
-    os.makedirs(outdir, exist_ok=True) 
 
     for result in results:
 
-        id = result.split('/')[-1].split('_')[0]
+        cyclone_id = result.split('/')[-1].split('_')[0]
 
-        periods_file = result+'/periods.csv'
-        try:
-            results_file = glob.glob(result+'/*_ERA5_track-15x15.csv')[0]
+        track_file = glob.glob(result+'/*track')[0]
+
+        #pd.read_csv(track_file, sep=';')
+
+        periods_fig_outfile = os.path.join(figures_outdir, f'RG{RG}_{cyclone_id}.png')
+        didatic_fig_outfile = os.path.join(didatic_outdir, f'RG{RG}_{cyclone_id}.png')
+        periods_csv_outfile = os.path.join(csv_outdir, f'RG{RG}_{cyclone_id}.csv')
+
+        options = {
+        "vorticity_column": 'min_zeta_850',
+        "plot": periods_fig_outfile,
+        "plot_steps": didatic_fig_outfile,
+        "export_dict": periods_csv_outfile,
+        "process_vorticity_args": {
+            "use_filter": "auto",
+            "use_smoothing_twice": "auto"}
+        }
+
+        # periods_file = result+'/periods.csv'
+        # try:
+        #     results_file = glob.glob(result+'/*_ERA5_track-15x15.csv')[0]
         
-            df = pd.read_csv(results_file, index_col=[0])
-            df['Datetime'] = pd.to_datetime(df.Date) + pd.to_timedelta(df.Hour, unit='h')
+        #     df = pd.read_csv(results_file, index_col=[0])
+        #     df['Datetime'] = pd.to_datetime(df.Date) + pd.to_timedelta(df.Hour, unit='h')
             
-            periods = pd.read_csv(periods_file, index_col=[0])
-            periods = periods.dropna()
-            for i in range(len(periods)):
-                start,end = periods.iloc[i]['start'],periods.iloc[i]['end']
-                selected_dates = df[(df['Datetime'] >= start) & (df['Datetime'] <= end)]
-                if i == 0:
-                    period = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
-                    period = period.to_frame(name=periods.iloc[i].name).transpose()
-                else:
-                    tmp = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
-                    tmp = tmp.to_frame(name=periods.iloc[i].name).transpose()
-                    period = pd.concat([period,tmp]) 
+        #     periods = pd.read_csv(periods_file, index_col=[0])
+        #     periods = periods.dropna()
+        #     for i in range(len(periods)):
+        #         start,end = periods.iloc[i]['start'],periods.iloc[i]['end']
+        #         selected_dates = df[(df['Datetime'] >= start) & (df['Datetime'] <= end)]
+        #         if i == 0:
+        #             period = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
+        #             period = period.to_frame(name=periods.iloc[i].name).transpose()
+        #         else:
+        #             tmp = selected_dates.drop(['Datetime','Date','Hour'],axis=1).mean()
+        #             tmp = tmp.to_frame(name=periods.iloc[i].name).transpose()
+        #             period = pd.concat([period,tmp]) 
 
-        except:
-            print(result)
-            pass
+        # except:
+        #     print(result)
+        #     pass
             
-        fname = f'{outdir}/{id}_ERA5.csv'
-        period.to_csv(fname)
-        print(f'{fname} created')
+        # fname = f'{outdir}/{id}_ERA5.csv'
+        # period.to_csv(fname)
+        # print(f'{fname} created')
