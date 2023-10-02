b28adc34861223389111ed664fd709aff7bdf026
daniloceano <danilo.oceano@gmail.com>
2023-10-01 23:48:01 -0300

    Periods for 1000 km

--- src_life-cycle/export_periods.py
+++ src_life-cycle/export_periods.py
@@ -130,7 +130,8 @@ testing = False
 # analysis_type = '70W' 
 # analysis_type = '48h'
 # analysis_type = '70W-48h'
-analysis_type = '70W-1000km'
+# analysis_type = '70W-1000km'
+analysis_type = '70W-1500km'
 
 print("Initializing periods analysis for: ", analysis_type) if not testing else print("Testing")
 
