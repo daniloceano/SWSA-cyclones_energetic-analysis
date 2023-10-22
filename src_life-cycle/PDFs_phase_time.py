f967d53e48406c382e2431e8be6ee8945c78ac0b
Danilo Couto de Souza <danilo.oceano@gmail.com>
2023-10-22 19:33:17 -0300

    Ridge plots for duration with matplotlib

--- src_life-cycle/PDFs_phase_time.py
+++ src_life-cycle/PDFs_phase_time.py
@@ -1,131 +1,221 @@
-import pandas as pd
+import os
 import glob
 import numpy as np
-import os
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import matplotlib.gridspec as grid_spec
+from sklearn.neighbors import KernelDensity
+from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
 
+# Constants
+SECONDS_IN_AN_HOUR = 3600
+
 def process_csv_file(csv_file):
-    df = pd.read_csv(csv_file, parse_dates=['start', 'end'], index_col=0)
-    phase_durations = {}
-    for phase in df.index:
-        start_time = df.loc[phase, 'start']
-        end_time = df.loc[phase, 'end']
-        duration_hours = (end_time - start_time).total_seconds() / 3600
-        phase_durations[phase] = duration_hours
-    return phase_durations
+    """
+    Process the CSV file and calculate phase durations.
+    
+    Args:
+    - csv_file (str): Path to the CSV file
+    
+    Returns:
+    - dict: Dictionary containing phase durations
+    """
+    try:
+        df = pd.read_csv(csv_file, parse_dates=['start', 'end'], index_col=0)
+        phase_durations = {
+            phase: ((df.loc[phase, 'end'] - df.loc[phase, 'start']).total_seconds() / SECONDS_IN_AN_HOUR)
+            for phase in df.index
+        }
+        return phase_durations
+    except Exception as e:
+        print(f"Error processing file {csv_file}: {e}")
+        return {}
+
 
 def process_phase_data_parallel(data_path):
-    csv_files = glob.glob(f'{data_path}*.csv')
-
-    durations = {}
+    """
+    Process CSV data in parallel.
     
-    # Use ThreadPoolExecutor to process files in parallel
+    Args:
+    - data_path (str): Path to the data directory
+    
+    Returns:
+    - dict: Aggregated durations for each phase
+    """
+    csv_files = glob.glob(os.path.join(data_path, '*.csv'))[:200]
+    durations = defaultdict(list)
+
     with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
         results = list(tqdm(executor.map(process_csv_file, csv_files), total=len(csv_files), desc='Processing Files'))
 
     for result in results:
         for phase, duration in result.items():
-            if phase not in durations:
-                durations[phase] = []
             durations[phase].append(duration)
 
     return durations
 
+
+def ensure_directory_exists(path):
+    """
+    Ensure a directory exists; if not, create it.
+    
+    Args:
+    - path (str): Directory path
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
 def create_seaborn_dataframe(duration_data, region):
-    data = {'Phase': [], 'Duration (hours)': [], 'Region': []}
+    """
+    Create a dataframe suitable for Seaborn plotting.
+    
+    Args:
+    - duration_data (dict): Data containing phase durations
+    - region (str): Region identifier
+    
+    Returns:
+    - DataFrame: Processed dataframe
+    """
+    data = {
+        'Phase': [],
+        'Duration (hours)': [],
+        'Region': region
+    }
 
     for phase, phase_durations in duration_data.items():
         data['Phase'].extend([phase] * len(phase_durations))
         data['Duration (hours)'].extend(phase_durations)
+
+    return pd.DataFrame(data)
+
+def plot_single_ridge(data, regions, phase, colors, fig, gs, figure_path):
+    """
+    Plot a single ridge plot for given data.
     
-    data['Region'] = region
-
-    df = pd.DataFrame(data)
-    return df
-
-def plot_ridge_plots(dfs, figure_path, phases, rgs):
-    for phase in phases:
-        sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
-
-        df = dfs[dfs['Phase'] == phase]
-        df_max = int(df["Duration (hours)"].max())
-
-        # Initialize the FacetGrid object
-        pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
-        pal= sns.color_palette("Set2", 12)
-        g = sns.FacetGrid(df, row="Region", hue="Region", aspect=15, height=.5, palette=pal)
-
-        # Draw the densities in a few steps
-        g.map(sns.kdeplot, "Duration (hours)",
-            bw_adjust=.5, clip_on=False,
-            fill=True, alpha=1, linewidth=1.5)
-        g.map(sns.kdeplot, "Duration (hours)", clip_on=False, color="w", lw=2, bw_adjust=.5)
-
-        # passing color=None to refline() uses the hue mapping
-        g.refline(y=0, linewidth=2, linestyle="-", color=None, clip_on=False)
-
-        # Define and use a simple function to label the plot in axes coordinates
-        def label(x, color, label):
-            ax = plt.gca()
-            ax.text(1, 0.2, label, fontweight="bold", color=color,
-                    ha="right", va="center", transform=ax.transAxes, zorder=100)
-        g.map(label, "Duration (hours)")
-
-        for i, (ax, region) in enumerate(zip(g.axes.flat, rgs)):
-            region = region if region else 'Total'
-            ax.set_xlim(0, max(df["Duration (hours)"]))  # Set the x-axis lower bound to 0
-            region_mean = df[df["Region"] == region]["Duration (hours)"].mean()
-            ax.axvline(x=region_mean, color="k", linestyle="-",
-                       label=f"Mean", alpha=0.8)
-
-            # Add vertical gridlines for ticks on the x-axis (e.g., every 10 hours)
-            for i in range(0, df_max, 5):
-                ax.axvline(x=i, color="k", linestyle="--", lw=0.25, alpha=0.5, zorder=1)
-
-        # Set the subplots to overlap
-        g.figure.subplots_adjust(hspace=-.25)
-
-        # Remove axes details that don't play well with overlap
-        g.set_titles("")
-        g.set(yticks=[], ylabel="")
-        g.despine(bottom=True, left=True)
+    Args:
+    - data (DataFrame): Data to plot
+    - regions (list): List of regions
+    - phase (str): Phase identifier
+    - colors (list): List of colors for the plot
+    - fig (Figure): Matplotlib figure object
+    - gs (GridSpec): Matplotlib GridSpec object
+    - figure_path (str): Path to save the figure
+    """
+    ax_objs = []
+    duration = data['Duration (hours)']
+    xmin, xmax = duration.min(), duration.max()
+    x_d = np.linspace(xmin, xmax, 1000)
+    
+    for idx, rg in enumerate(regions):
+        x = np.array(data[data['Region'] == rg]['Duration (hours)'])
         
-        # Save the ridge plot for the phase
+        kde = KernelDensity(bandwidth=1, kernel='gaussian')
+        kde.fit(x[:, None])
+
+        logprob = kde.score_samples(x_d[:, None])
+        ax_objs.append(fig.add_subplot(gs[idx:idx+1, 0:]))
+
+        # plotting the distribution
+        ax_objs[-1].plot(x_d, np.exp(logprob), color="#f0f0f0", lw=1)
+        ax_objs[-1].fill_between(x_d, np.exp(logprob), alpha=1, color=colors[idx % len(colors)])
+
+        # setting uniform x and y lims
+        ax_objs[-1].set_xlim(xmin, xmax)
+        ax_objs[-1].set_ylim(0, np.exp(logprob).max() + 0.2)  # added a bit of padding
+
+        # make background transparent
+        rect = ax_objs[-1].patch
+        rect.set_alpha(0)
+
+        # remove borders, axis ticks, and labels
+        ax_objs[-1].set_yticklabels([])
+        ax_objs[-1].yaxis.set_ticks([])  # Remove y-axis ticks
+
+        if idx == len(regions) - 1:
+            ax_objs[-1].set_xlabel("Duration (hours)", fontsize=16, fontweight="bold")
+        else:
+            ax_objs[-1].set_xticklabels([])
+
+        # Determine the y-limits for the grid lines, which would be confined to the KDE
+        y_limit = np.exp(logprob).max() + 0.2
+
+        # Calculate grid line positions
+        num_lines = 10
+        interval = round(xmax) // num_lines
+        interval = max(1, interval) + 1 
+        
+        # Draw each grid line manually using axvline
+        for grid_x in range(0, int(xmax), interval):
+            ax_objs[-1].axvline(x=grid_x, ymin=0, ymax=y_limit, color='grey', linestyle='--', linewidth=0.5)
+
+        # Calculate the mean of the data for the current region
+        mean_value = x.mean()
+
+        # Draw a vertical line at the mean value
+        ax_objs[-1].axvline(x=mean_value, ymin=0, ymax=np.exp(logprob).max() + 0.1, 
+                            color='black', linestyle='-', linewidth=2, label="Mean")
+
+        spines = ["top", "right", "left", "bottom"]
+        for s in spines:
+            ax_objs[-1].spines[s].set_visible(False)
+
+        ax_objs[-1].text(-0.02, 0, rg, fontweight="bold", fontsize=14, ha="right")
+
+        gs.update(hspace=-0.7)
+        fig.text(0.07, 0.75, phase, fontsize=20)
+        plt.tight_layout()
         plt.savefig(os.path.join(figure_path, f'Ridge_Plot_{phase}.png'))
 
+def plot_ridge_plots(dfs, figure_path, phases):
+    """
+    Plot ridge plots for the given data.
+    
+    Args:
+    - dfs (DataFrame): Data to plot
+    - figure_path (str): Path to save the plots
+    - phases (list): List of phase identifiers
+    """
+    colors = ['#3333ff', '#4040ff', '#5959cc', '#737399', '#868699', '#a68099', '#b38699', '#c79999']
+    
+    for phase in phases:
+        data = dfs[dfs['Phase'] == phase]
+        regions = dfs['Region'].unique()
+        gs = grid_spec.GridSpec(len(regions), 1)
+        fig = plt.figure(figsize=(12, 9))
+        plot_single_ridge(data, regions, phase, colors, fig, gs, figure_path)
+
 def main():
+    analysis_type_to_regions = {
+        'BY_RG-all': ['RG1', 'RG2', 'RG3', 'all_RG'],
+        '70W-no-continental': [False, "ARG", "LA-PLATA", "SE-BR", "SE-SAO", "AT-PEN", "WEDDELL", "SA-NAM"],
+        'default': ['']
+    }
+
     analysis_type = '70W-no-continental'
-
-    figure_path = f'../figures/periods_statistics/{analysis_type}/phase_time/'
-    if not os.path.exists(figure_path):
-        os.makedirs(figure_path)
-
-    if analysis_type == 'BY_RG-all':
-        rgs = ['RG1', 'RG2', 'RG3', 'all_RG']
-    elif analysis_type == '70W-no-continental':
-        rgs = [False, "ARG", "LA-PLATA", "SE-BR", "SE-SAO", "AT-PEN", "WEDDELL", "SA-NAM"]
-    else:
-        rgs = ['']
+    regions = analysis_type_to_regions.get(analysis_type, analysis_type_to_regions['default'])
+    
+    figure_path = os.path.join('..', 'figures', 'periods_statistics', analysis_type, 'phase_time')
+    ensure_directory_exists(figure_path)
 
     data_frames = []
-    for rg in rgs:
-        rg_str = f'_{rg}' if rg else '_SAt'
-        data_path = f'../periods-energetics/{analysis_type}{rg_str}/' if rg else f'../periods-energetics/{analysis_type}/'
+    for region in regions:
+        region_str = f'_{region}' if region else '_SAt'
+        data_path = os.path.join('..', 'periods-energetics', analysis_type + region_str)
+        
         duration_data = process_phase_data_parallel(data_path)
-        print(f'Processed data for RG: {rg}')
+        print(f'Processed data for region: {region}')
 
-        # Create a DataFrame for Seaborn
-        region = rg if rg else 'Total'
-        df = create_seaborn_dataframe(duration_data, region)
+        df = create_seaborn_dataframe(duration_data, region if region else 'Total')
         data_frames.append(df)
 
-    phases = list(duration_data.keys())  # Get the list of phases from the last processed data
+    phases = list(duration_data.keys())
     dfs = pd.concat(data_frames)
-    # Plot ridge plots for each phase
-    plot_ridge_plots(dfs, figure_path, phases, rgs)
+
+    plot_ridge_plots(dfs, figure_path, phases)
 
 if __name__ == '__main__':
     main()
