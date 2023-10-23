ffe45af47b27c7a28824ff5e3aa99c32b6657ea8
Danilo Couto de Souza <danilo.oceano@gmail.com>
2023-10-23 11:05:35 -0300

    Exporting csv before plotting

--- src_life-cycle/PDFs_phase_time.py
+++ src_life-cycle/PDFs_phase_time.py
@@ -28,14 +28,18 @@ def process_csv_file(csv_file):
         df = pd.read_csv(csv_file, parse_dates=['start', 'end'], index_col=0)
         
         # Deduce season from the 'start' date
-        month = df.loc['incipient', 'start'].month
-        season = 'DJF' if month in [12, 1, 2] else ('JJA' if month in [6, 7, 8] else None)
+        month = df['start'].min().month
+        if month in  [12, 1, 2, 6, 7, 8]:
+            season = 'DJF' if month in [12, 1, 2] else 'JJA'
         
-        phase_durations = {
-            phase: ((df.loc[phase, 'end'] - df.loc[phase, 'start']).total_seconds() / SECONDS_IN_AN_HOUR)
-            for phase in df.index
-        }
-        return {**phase_durations, "Season": season}
+            phase_durations = {
+                phase: ((df.loc[phase, 'end'] - df.loc[phase, 'start']).total_seconds() / SECONDS_IN_AN_HOUR)
+                for phase in df.index
+            }
+            return {**phase_durations, "Season": season}
+        else:
+            return {}
+        
     except Exception as e:
         print(f"Error processing file {csv_file}: {e}")
         return {}
@@ -106,18 +110,27 @@ def plot_single_ridge(data, regions, figure_path, phase):
     Plot a single ridge plot for given data, combining both seasons in one subplot.
     """
     # Create a figure with a row for each region
-    fig, ax = plt.subplots(nrows=len(data['Region'].unique()), ncols=2, figsize=(12, 9))
+    gs = grid_spec.GridSpec(len(regions), 2)
+    fig = plt.figure(figsize=(12, 9))
     
     duration = data['Duration (hours)']
     xmin, xmax = duration.min(), duration.max()
-    x_d = np.linspace(xmin, xmax, 1000)
 
-    # Define the vertical gap between rows to create overlapping effect
-    vertical_gap = 0.07 
-    horizontal_gap = 0.01
+    bandwidth = {
+        "incipient": 1,
+        "intensification": 1.5,
+        "mature": 1,
+        "decay": 1.5,
+        "intensification 2": 2,
+        "mature 2": 1,
+        "decay 2": 2,
+        "residual": 1.5,
+        }
+    
+    x_d = np.linspace(xmin, xmax, 500)
 
-    blue_shades = ['#010339','#03045e','#023e8a','#0077b6','#0096c7','#00b4d8','#48cae4','#a4defb']
-    red_shades = ['#1c0008','#370617','#6a040f','#9d0208','#d00000','#dc2f02','#e85d04','#ff9066']
+    red_shades = ['#9d0208', '#b30109', '#c7010a', '#d9000b', '#dc2f02', '#e14c03', '#e56904', '#ff9066']
+    blue_shades = ['#023e8a', '#0251a0', '#0264b6', '#0077b6', '#0091c3', '#00abd0', '#00c5dd', '#48cae4']
 
     for idx, rg in enumerate(regions):
         color_djf = red_shades[idx]
@@ -126,26 +139,28 @@ def plot_single_ridge(data, regions, figure_path, phase):
             season_data = data[(data['Region'] == rg) & (data['Season'] == season)]
             x = np.array(season_data['Duration (hours)'])
             
-            kde = KernelDensity(bandwidth=1, kernel='gaussian')
+            kde = KernelDensity(bandwidth=bandwidth[phase], kernel='gaussian')
             kde.fit(x[:, None])
 
             color = color_djf if season == "DJF" else color_jja
 
+            ax = fig.add_subplot(gs[idx:idx+1, col])
+
             logprob = kde.score_samples(x_d[:, None])
-            ax[idx][col].plot(x_d, np.exp(logprob), color="#f0f0f0", lw=1, linestyle='-')
-            ax[idx][col].fill_between(x_d, np.exp(logprob), alpha=1, color=color, linestyle='-')
+            ax.plot(x_d, np.exp(logprob), color="#f0f0f0", lw=1, linestyle='-')
+            ax.fill_between(x_d, np.exp(logprob), alpha=1, color=color, linestyle='-')
             
             # setting uniform x and y lims
-            ax[idx][col].set_xlim(xmin, xmax)
-            ax[idx][col].set_ylim(0, np.exp(logprob).max() + 0.2)  # added a bit of padding
+            ax.set_xlim(xmin, xmax)
+            ax.set_ylim(0, np.exp(logprob).max() + 0.2)  # added a bit of padding
 
             # make background transparent
-            rect = ax[idx][col].patch
+            rect = ax.patch
             rect.set_alpha(0)
 
             # remove borders, axis ticks, and labels
-            ax[idx][col].set_yticklabels([])
-            ax[idx][col].yaxis.set_ticks([])  # Remove y-axis ticks
+            ax.set_yticklabels([])
+            ax.yaxis.set_ticks([])  # Remove y-axis ticks
 
             # Determine the y-limits for the grid lines, which would be confined to the KDE
             y_limit = np.exp(logprob).max() + 0.2
@@ -157,45 +172,47 @@ def plot_single_ridge(data, regions, figure_path, phase):
             
             # Draw each grid line manually using axvline
             for grid_x in range(0, int(xmax), interval):
-                ax[idx][col].axvline(x=grid_x, ymin=0, ymax=y_limit, color='grey', linestyle='--', linewidth=0.5)
+                ax.axvline(x=grid_x, ymin=0, ymax=y_limit, color='grey', linestyle='--', linewidth=0.5)
 
             # Calculate the mean of the data for the current region
             mean_value = x.mean()
 
             color_line = 'r' if season == "DJF" else 'b'
             # Draw a vertical line at the mean value
-            ax[idx][col].axvline(x=mean_value, ymin=0, ymax=np.exp(logprob).max() + 0.1, 
-                                color=color_line, linestyle='-', linewidth=2, label="Mean")
+            ax.axvline(x=mean_value, ymin=0, ymax=np.exp(logprob).max() + 0.2, 
+                                color='k', linestyle='-', linewidth=2, label="Mean")
 
             spines = ["top", "right", "left", "bottom"]
             for s in spines:
-                ax[idx][col].spines[s].set_visible(False)
+                ax.spines[s].set_visible(False)
+
             if col == 0:
-                ax[idx][col].text(-0.02, 0, rg, fontweight="bold", fontsize=14, ha="right")
+                ax.text(-0.5, 0, rg, fontweight="bold", fontsize=14, ha="right")
             
-            # x-label
-            if idx == len(regions) - 1:
-                fig.text(0.5, 0.55, 'Duration (hours)', ha='center', fontsize=16, fontweight="bold")
-            ax[idx][col].set_xticklabels([])
+            if idx != len(regions)-1:                
+                ax.set_xticklabels([])
 
             # season
             if idx == 0:
                 letter = "A" if col == 0 else "B"
-                ax[idx][col].text(10, 0.2, f"({letter}) {season}", fontweight="bold", fontsize=14, ha="right")
+                ax.text(10, 0.2, f"({letter}) {season}", fontweight="bold", fontsize=14, ha="right")
 
             # Adjust x-position of the second column to make columns closer
             if col == 1:
-                pos = ax[idx][col].get_position()
-                ax[idx][col].set_position([pos.x0 + horizontal_gap, pos.y0, pos.width, pos.height])
+                pos = ax.get_position()
+                ax.set_position([pos.x0 + 0.01, pos.y0, pos.width, pos.height])
 
-        # Adjust the position of each row of axes to create overlap
-        pos = ax[idx][0].get_position()
-        ax[idx][0].set_position([pos.x0, pos.y0 + idx * vertical_gap, pos.width, pos.height])
-        ax[idx][1].set_position([pos.x0 + pos.width + horizontal_gap, pos.y0 + idx * vertical_gap, pos.width, pos.height])
+    fig.text(0.5, 0.05, 'Duration (hours)', ha='center', fontsize=16, fontweight="bold")
+
+    # Adjust the position of each row of axes to create overlap
+    gs.update(hspace=-0.7)
+    gs.update(wspace=0.1)
+    plt.tight_layout()
 
     # Save the combined figure for the current phase
-    # plt.tight_layout()
-    plt.savefig(os.path.join(figure_path, f'Ridge_Plot_{phase}.png'))
+    fname = os.path.join(figure_path, f'Ridge_Plot_{phase}.png')
+    plt.savefig(fname, dpi=200)
+    print(f"{fname} created.")
 
 def plot_ridge_plots(dfs, figure_path, phases):
     for phase in phases:
@@ -206,7 +223,6 @@ def plot_ridge_plots(dfs, figure_path, phases):
         regions = dfs['Region'].unique()
             
         plot_single_ridge(data, regions, figure_path, phase)
-
 
 def main():
     analysis_type_to_regions = {
@@ -221,19 +237,28 @@ def main():
     figure_path = os.path.join('..', 'figures', 'periods_statistics', analysis_type, 'phase_time')
     ensure_directory_exists(figure_path)
 
-    data_frames = []
-    for region in regions:
-        region_str = f'_{region}' if region else ''
-        data_path = os.path.join('..', 'periods-energetics', analysis_type + region_str)
-        
-        duration_data = process_phase_data_parallel(data_path)
-        print(f'Processed data for region: {region}')
+    phase_time_database = f"../periods_species_statistics/{analysis_type}/phase_time.csv"
 
-        df = create_seaborn_dataframe(duration_data, region if region else 'Total')
-        data_frames.append(df)
+    try:
+        pd.read_csv(phase_time_database)
 
-    phases = [key for key in list(duration_data.keys()) if key not in ['Season', 'incipient 2']]
-    dfs = pd.concat(data_frames)
+    except:
+        print(f"{phase_time_database} not found, creating it...")
+        data_frames = []
+        for region in regions:
+            region_str = f'_{region}' if region else ''
+            data_path = os.path.join('..', 'periods-energetics', analysis_type + region_str)
+            
+            duration_data = process_phase_data_parallel(data_path)
+            print(f'Processed data for region: {region}')
+
+            df = create_seaborn_dataframe(duration_data, region if region else 'Total')
+            data_frames.append(df)
+
+        phases = [key for key in list(duration_data.keys()) if key not in ['Season', 'incipient 2']]
+        dfs = pd.concat(data_frames)
+        dfs.to_csv(phase_time_database)
+        print(f"{phase_time_database} created.")
 
     plot_ridge_plots(dfs, figure_path, phases)
 
