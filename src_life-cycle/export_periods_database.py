ad9080180bd9ac20b80afa91ca57fda1653a27e9
Danilo Couto de Souza <danilo.oceano@gmail.com>
2023-10-28 14:22:02 -0300

    Fix

--- src_life-cycle/export_periods_database.py
+++ src_life-cycle/export_periods_database.py
@@ -6,7 +6,7 @@
 #    By: Danilo  <danilo.oceano@gmail.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/10/27 19:48:00 by Danilo            #+#    #+#              #
-#    Updated: 2023/10/28 14:18:26 by Danilo           ###   ########.fr        #
+#    Updated: 2023/10/28 14:21:06 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -170,7 +170,7 @@ def main():
     tracks['date'] = pd.to_datetime(tracks['date']) 
     tracks['year'] = tracks['date'].dt.year
     unique_years = tracks['year'].unique()
-    os.makedirs(duration_database_directory, exist_ok=True)
+    os.makedirs(duration_database, exist_ok=True)
     for year in unique_years:
         print(f"Processing year: {year}")
         tracks_year = tracks[tracks['year'] == year]
