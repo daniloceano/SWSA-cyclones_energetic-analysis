da823f2497f3c7620c36b494b829f695b162cc06
daniloceano <danilo.oceano@gmail.com>
2023-10-30 18:27:31 -0300

    Updated statistics

--- src_life-cycle/periods_statistics.py
+++ src_life-cycle/periods_statistics.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    periods_statistics.py                              :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: Danilo <danilo.oceano@gmail.com>           +#+  +:+       +#+         #
+#    By: daniloceano <daniloceano@student.42.fr>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/10/30 16:09:48 by Danilo            #+#    #+#              #
-#    Updated: 2023/10/30 17:29:54 by Danilo           ###   ########.fr        #
+#    Updated: 2023/10/30 18:25:59 by daniloceano      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,6 +27,7 @@ from concurrent.futures import ThreadPoolExecutor
 SECONDS_IN_AN_HOUR = 3600
 ALPHA = 0.05  # Significance level
 ANALYSIS_TYPE = '70W-no-continental'
+METRICS = ['Total Time (Hours)', 'Total Distance (km)', 'Mean Speed (m/s)']
 
 def get_database():
     files = f"../periods_species_statistics/{ANALYSIS_TYPE}/periods_database/periods_database_*.csv"
@@ -41,70 +42,13 @@ def get_database():
     database = database.dropna(subset=['Mean Speed (m/s)'])
     return database
 
-def process_csv_file(csv_file):
-    """
-    Process the CSV file and calculate phase durations.
-    
-    Args:
-    - csv_file (str): Path to the CSV file
-    
-    Returns:
-    - dict: Dictionary containing phase durations and seasons
-    """
-    try:
-        df = pd.read_csv(csv_file, parse_dates=['start', 'end'], index_col=0)
-        
-        # Check if mature phase is present
-        if 'mature' in df.index:
-            mature_month = df.at['mature', 'start'].month
-            
-            # Deduce season from the 'start' date of mature phase
-            if mature_month in  [12, 1, 2, 6, 7, 8]:
-                season = 'DJF' if mature_month in [12, 1, 2] else 'JJA'
-                
-                phase_durations = {
-                    phase: ((df.at[phase, 'end'] - df.at[phase, 'start']).total_seconds() / SECONDS_IN_AN_HOUR)
-                    for phase in df.index
-                }
-                return {**phase_durations, "Season": season}
-        
-        return {}
-        
-    except Exception as e:
-        print(f"Error processing file {csv_file}: {e}")
-        return {}
-    
-def process_phase_data_parallel(data_path):
-    """
-    Process CSV data in parallel.
-    
-    Args:
-    - data_path (str): Path to the data directory
-    
-    Returns:
-    - dict: Aggregated durations for each phase
-    """
-    csv_files = glob.glob(os.path.join(data_path, '*.csv'))
-    durations = defaultdict(list)
-
-    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-        results = list(tqdm(executor.map(process_csv_file, csv_files), total=len(csv_files), desc='Processing Files'))
-
-    for result in results:
-        for phase, duration in result.items():
-            durations[phase].append(duration)
-
-    return durations
-
-def ensure_directory_exists(path):
-    """
-    Ensure a directory exists; if not, create it.
-    
-    Args:
-    - path (str): Directory path
-    """
-    if not os.path.exists(path):
-        os.makedirs(path)
+def metric_to_formatted_string(metric):
+    mapping = {
+        'Total Time (Hours)': 'total_time',
+        'Total Distance (km)': 'total_distance',
+        'Mean Speed (m/s)': 'mean_speed'
+    }
+    return mapping.get(metric, '')
 
 def compare_djf_jja(data_for_region):
     # Extract DJF and JJA data
@@ -134,7 +78,7 @@ def compare_djf_jja(data_for_region):
 
     return test_used, p_value
 
-def plot_single_ridge(data, regions, figure_path, phase):
+def plot_single_ridge(data, regions, figure_path, phase, metric):
     """
     Plot a single ridge plot for given data, combining both seasons in one subplot.
     """
@@ -142,7 +86,7 @@ def plot_single_ridge(data, regions, figure_path, phase):
     gs = grid_spec.GridSpec(len(regions), 2)
     fig = plt.figure(figsize=(12, 9))
 
-    duration = data['Total Time (Hours)']
+    variable = data['Total Time (Hours)']
 
     # [bandwidth, number of samples, quantile]
     kde_params = {
@@ -157,7 +101,7 @@ def plot_single_ridge(data, regions, figure_path, phase):
     }
 
     # Calculate the upper percentile to define the x-axis range
-    upper_percentile = np.percentile(duration, kde_params[phase][2]) 
+    upper_percentile = np.percentile(variable, kde_params[phase][2]) 
     
     # Set the x-axis limits based on the percentiles
     xmin, xmax = 0, upper_percentile
@@ -256,7 +200,9 @@ def plot_single_ridge(data, regions, figure_path, phase):
             if col == 1:
                 pos = ax.get_position()
                 ax.set_position([pos.x0 + 0.01, pos.y0, pos.width, pos.height])
-    fig.text(0.5, 0.05, f'Duration (hours) - {phase}', ha='center', fontsize=16, fontweight="bold")
+    # Update the title to reflect the metric being analyzed
+    metric_name = metric.replace("(", "").replace(")", "").lower()  # Remove any parenthesis
+    fig.text(0.5, 0.05, f'{metric_name} - {phase}', ha='center', fontsize=16, fontweight="bold")
     gs.update(hspace=-0.7)
     gs.update(wspace=0.1)
     fig.subplots_adjust(top=1.1)
@@ -266,61 +212,55 @@ def plot_single_ridge(data, regions, figure_path, phase):
     print(f"{fname} created.")
 
 def plot_ridge_plots(database, phases):
-    figure_path = os.path.join('..', 'figures', 'periods_statistics', ANALYSIS_TYPE, 'phase_time')
-    ensure_directory_exists(figure_path)
-    for phase in phases:
-        print(f"\n-----------------\nPlotting phase: {phase}")
-        data = database[database['phase'] == phase]
-        regions = database['Region'].unique()
-        plot_single_ridge(data, regions, figure_path, phase)
-    # phase = "residual"
-    # data = database[database['Phase'] == phase]
-    # plot_single_ridge(data, regions, figure_path, phase)
+    for metric in METRICS:
+        for phase in phases:    
+            metric_formatted = metric_to_formatted_string(metric)
+            figure_path = os.path.join('..', 'figures', 'periods_statistics', ANALYSIS_TYPE, metric_formatted)
+            os.makedirs(figure_path, exist_ok=True)
+            print(f"\n-----------------\nPlotting phase: {phase} for {metric}")
+            data = database[database['phase'] == phase]
+            regions = database['Region'].unique()
+            plot_single_ridge(data, regions, figure_path, phase, metric) 
 
 def phases_statistics(database):
     # Specified order for 'Region'
     region_order = ["Total", "ARG", "LA-PLATA", "SE-BR", "SE-SAO", "AT-PEN", "WEDDELL", "SA-NAM"]
     database['Region'] = pd.Categorical(database['Region'], categories=region_order, ordered=True)
-    # Group by 'Region', 'Season' and 'Phase' and compute mean and std
-    grouped = database.groupby(['Region', 'Season', 'phase'])['Total Time (Hours)'].agg(['mean', 'std']).reset_index()
-    # Pivot the dataframe to have 'phase' as columns
-    pivot_df = grouped.pivot_table(index=['Region', 'Season'], columns='phase', values=['mean', 'std'], aggfunc='first')
-    # Flatten MultiIndex columns and rename them
-    pivot_df.columns = ['_'.join(col).strip() for col in pivot_df.columns.values]
-    pivot_df.reset_index(inplace=True)
-    # Given phases in the desired order
-    phase_order = ["incipient", "intensification", "mature", "decay", 
-                "intensification 2", "mature 2", "decay 2", "residual"]
-    # Create an ordered list of columns based on the desired order with mean followed by std
-    ordered_columns = ["Region", "Season"]
-    for phase in phase_order:
-        ordered_columns.append(f"mean_{phase}")
-        ordered_columns.append(f"std_{phase}")
-    # Reorder the dataframe columns
-    pivot_df = pivot_df[ordered_columns]
-    # Create a custom order for the 'Season' column
-    season_order = ["DJF", "JJA"]
-    pivot_df['Season'] = pd.Categorical(pivot_df['Season'], categories=season_order, ordered=True)
-    # Sort by 'Season' first and then by 'Region'
-    pivot_df.sort_values(by=['Season', 'Region'], inplace=True)
-    # Identify columns that contain numerical data (excluding 'Region' and 'Season' columns)
-    numeric_columns = pivot_df.columns.difference(['Region', 'Season'])
-    # Round the numerical columns to 1 decimal place
-    pivot_df[numeric_columns] = pivot_df[numeric_columns].round(1)
-    # Export results
-    filename = f"../periods_species_statistics/{ANALYSIS_TYPE}/statistics/phase_time_stastics.csv"
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    pivot_df.to_csv(filename, index=False)
+    # Expanded loop to iterate over each metric
+    for metric in METRICS:
+        # Group by 'Region', 'Season' and 'Phase' and compute mean and std for the metric
+        grouped = database.groupby(['Region', 'Season', 'phase'])[metric].agg(['mean', 'std']).reset_index()
+        # Pivot the dataframe to have 'phase' as columns
+        pivot_df = grouped.pivot_table(index=['Region', 'Season'], columns='phase', values=['mean', 'std'], aggfunc='first')
+        # Flatten MultiIndex columns and rename them
+        pivot_df.columns = ['_'.join(col).strip() for col in pivot_df.columns.values]
+        pivot_df.reset_index(inplace=True)
+        # Given phases in the desired order
+        phase_order = ["incipient", "intensification", "mature", "decay", 
+                    "intensification 2", "mature 2", "decay 2", "residual"]
+        # Create an ordered list of columns based on the desired order with mean followed by std
+        ordered_columns = ["Region", "Season"]
+        for phase in phase_order:
+            ordered_columns.append(f"mean_{phase}")
+            ordered_columns.append(f"std_{phase}")
+        # Reorder the dataframe columns
+        pivot_df = pivot_df[ordered_columns]
+        # Create a custom order for the 'Season' column
+        season_order = ["DJF", "JJA"]
+        pivot_df['Season'] = pd.Categorical(pivot_df['Season'], categories=season_order, ordered=True)
+        # Sort by 'Season' first and then by 'Region'
+        pivot_df.sort_values(by=['Season', 'Region'], inplace=True)
+        # Identify columns that contain numerical data (excluding 'Region' and 'Season' columns)
+        numeric_columns = pivot_df.columns.difference(['Region', 'Season'])
+        # Round the numerical columns to 1 decimal place
+        pivot_df[numeric_columns] = pivot_df[numeric_columns].round(1)
+        # Export results
+        metric_formatted = metric_to_formatted_string(metric)
+        filename = f"../periods_species_statistics/{ANALYSIS_TYPE}/statistics/stastics_{metric_formatted}.csv"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        pivot_df.to_csv(filename, index=False)
 
 def main():
-    analysis_type_to_regions = {
-        'BY_RG-all': ['RG1', 'RG2', 'RG3', 'all_RG'],
-        '70W-no-continental': [False, "ARG", "LA-PLATA", "SE-BR", "SE-SAO", "AT-PEN", "WEDDELL", "SA-NAM"],
-        'default': ['']
-    }
-    # Get regions by analysis type
-    regions = analysis_type_to_regions[ANALYSIS_TYPE]
-    # Get all data collected previously
     database = get_database()
     # Do not use incipient 2, as it should not exist in the data
     phases = [phase for phase in database['phase'].unique() if phase != 'incipient 2']
